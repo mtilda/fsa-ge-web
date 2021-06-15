e10b998b44f7aaa8d4614c984051667b9f761e9b
Mathilda <me@mathilda.dev>
2021-06-15 00:48:23 -0400

    Import font

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import '@fontsource/roboto';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
